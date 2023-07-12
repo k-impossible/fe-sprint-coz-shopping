da098e7f6064b9ee7a265126c615e3ed90a97ed0
t.h.kim <kth3349@gmail.com>
2023-07-12 17:42:29 +0900

    Revert "[Feat] Main 페이지 작업"

--- solo-project/src/App.js
+++ solo-project/src/App.js
@@ -1,33 +1,11 @@
-import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
-import Main from "./pages/Main";
-import BookMarkList from "./pages/BookMarkList";
-import ProductList from "./pages/ProductList";
-import Footer from "./components/Footer";
-import GlobalStyle from "./GlobalStyle";
-import styled from "styled-components";
 
-const Wrap = styled.div`
-  padding: 70px;
-  min-height: calc(100vh - 60px);
-  padding-top: 150px;
-`;
+import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
-      <GlobalStyle />
-      <Header />
-      <Wrap>
-        <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="/productList" element={<ProductList />} />
-          <Route path="/bookmarkList" element={<BookMarkList />} />
-        </Routes>
-      </Wrap>
-      <Footer />
-    </BrowserRouter>
+    <div className="App">
+      
+    </div>
   );
 }
 
--- solo-project/src/pages/BookMarkList.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function BookMarkList(){
-    return (
-        <div>bookmark list</div>
-    )
-}
