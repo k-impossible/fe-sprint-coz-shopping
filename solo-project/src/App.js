1041c99d9b3fd1fbb11bdabb7faaf91ad1c581b1
k-impossible <kth3349@gmail.com>
2023-07-12 16:37:07 +0900

    Header,Footer,Main,Item - styled components 적용 완료

--- solo-project/src/App.js
+++ solo-project/src/App.js
@@ -1,21 +1,33 @@
-import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from "./components/Header"
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
 import Main from "./pages/Main";
 import BookMarkList from "./pages/BookMarkList";
 import ProductList from "./pages/ProductList";
-import Footer from "./components/Footer"
+import Footer from "./components/Footer";
+import GlobalStyle from "./GlobalStyle";
+import styled from "styled-components";
+
+const Wrap = styled.div`
+  padding: 70px;
+  min-height: calc(100vh - 60px);
+  padding-top: 150px;
+`;
+
 function App() {
   return (
-    <Router>
-      <Header/>
-      <Routes>
-        <Route path="/" element={<Main/>}/>
-        <Route path="/productList" element={<ProductList/>}/>
-        <Route path="/bookmarkList" element={<BookMarkList/>}/>
-      </Routes>
-      <Footer/>
-    </Router>
+    <BrowserRouter>
+      <GlobalStyle />
+      <Header />
+      <Wrap>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/productList" element={<ProductList />} />
+          <Route path="/bookmarkList" element={<BookMarkList />} />
+        </Routes>
+      </Wrap>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
--- solo-project/src/components/Footer.js
+++ solo-project/src/components/Footer.js
@@ -1,10 +1,22 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
 
-export default function Footer(){
-    return (
-        <div className='footer-container'>
-            <p>개인정보 처리방침 | 이용약관</p>
-            <p>All rights reserved @ Codestates</p>
-        </div>
-    )
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  color: #9e9e9e;
+  font-size: 12px;
+  border-top: 1px solid #e0e0e0;
+  padding: 20px 0;
+  height: 60px;
+`;
+export default function Footer() {
+  return (
+    <Container>
+      <p>개인정보 처리방침 | 이용약관</p>
+      <p>All rights reserved @ Codestates</p>
+    </Container>
+  );
 }
